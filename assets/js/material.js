a40d59430e28356662d0fe60600a0b7e68ba8cc6
makeupwith <i2086051@cc.kyoto-su.ac.jp>
2020-09-02 05:57:53 +0900

    Add head edit and Fix somethings

--- assets/js/material.js
+++ assets/js/material.js
@@ -1,4 +1,35 @@
-//-- Buttons --
+/* 
+*** Material Design Components ***
+1. App Bars: top
+2. Buttons
+3. Buttons: floating action button
+4. Buttons: icon button
+5. Cards
+6. Chips
+7. Data tabels
+8. Dialogs
+9. Image list
+10. Lists
+11. Menus
+12. Navigaton drawer
+13. Progress indicators
+14. Selection controls: checkboxes
+15. Selection controls: radio buttons
+16. Selection controls: switches
+17. Sliders
+18. Snackbars
+19. Tabs
+20. Text fields
+E1. Selects
+*/
+
+
+/* -- 1. App Bars: top -- */
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(topAppBarElement);
+
+
+/* -- 2. Buttons -- */
 // - Eye - 
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.eye-button'));
 // - Mouse - 
@@ -6,7 +37,17 @@ mdc.ripple.MDCRipple.attachTo(document.querySelector('.mouse-button'));
 // - head - 
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.head-button'));
 
-//-- Slider --
+
+/* -- 8. Dialogs -- */
+const dialog = mdc.dialog.MDCDialog.attachTo(document.querySelector('.mdc-dialog'));
+// const list = mdc.list.MDCList.attachTo(document.querySelector('.mdc-dialog .mdc-list'));
+dialog.listen('MDCDialog:opened', () => {
+  console.log("open dailog")
+  // list.layout();
+});
+
+
+/* -- 17. Sliders -- */
 // - Eye - 
 const sliderH = mdc.slider.MDCSlider.attachTo(document.querySelector('.eye-size-h'));
 sliderH.listen('MDCSlider:change', () => console.log(`Value changed to ${sliderH.value}`));
@@ -17,15 +58,18 @@ pupilS.listen('MDCSlider:change', () => console.log(`Value changed to ${pupilS.v
 const mouseSize = mdc.slider.MDCSlider.attachTo(document.querySelector('.mouse-size'));
 mouseSize.listen('MDCSlider:change', () => console.log(`Value changed to ${mouseSize.value}`));
 // - Head -
-const headSize = mdc.slider.MDCSlider.attachTo(document.querySelector('.head-size'));
-mouseSize.listen('MDCSlider:change', () => console.log(`Value changed to ${headSize.value}`));
+// const headSize = mdc.slider.MDCSlider.attachTo(document.querySelector('.head-size'));
+// mouseSize.listen('MDCSlider:change', () => console.log(`Value changed to ${headSize.value}`));
 
-//-- Select --
+
+/* -- 18. Tabs -- */
+const tabBar = mdc.tabBar.MDCTabBar.attachTo(document.querySelector('.mdc-tab-bar'));
+tabBar.listen('MDCTABBAR:change', () => console.log(`Value changed to ${tabBar.value}`));
+
+
+/* -- E1. Selects -- */
 // - Eye - 
 const eyeSelect = new mdc.select.MDCSelect.attachTo(document.querySelector('.eye-select'));
-// eyeSelect.listen('MDCSelect:change', () => {
-//   alert(`Selected option at index ${eyeSelect.selectedIndex} with value "${eyeSelect.value}"`);
-// });
 // - Mouse - 
 const mouseSelect = new mdc.select.MDCSelect.attachTo(document.querySelector('.mouse-select'));
 // - Head -
--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -5,20 +5,30 @@
 */
 
 
-/*** -- 1. Class definition -- ***/ 
+/*** -- 1. Class definition -- ***/
 /* 
 1.1 HEAD
+ .1.0 definition of canvas
  .1.a eye
  .1.b mouse
+ .1.c head
 1.2
+1.x Global variable
+ .x.a eye
+ .x.b mouse
 */
 
-// キャンバスの定義
+
+/* -- 1.1 HEAD -- */
+
+// .1.0 Definition of canvas
 const canvasColor = '#ff980099';
-const canvasX = 598;
+const canvasX = 598
+// const canvasX = window.innerWidth - 758;
 const canvasY = 520;
+console.log(window.innerWidth);
 
-
+// .1.a Eye
 class eye {
   constructor(x, y, width, height, eyeScale, lor) {
     this.x = x;
@@ -31,10 +41,14 @@ class eye {
   }
 
   init(pattern) {
-    if(pattern == "パターン１") rightEye = new eye(199, 260, 50, 50, 1.0, "rightEye"),leftEye = new eye(399, 260, 50, 50, 1.0, "leftEye"); 
-    if(pattern == "パターン２") rightEye = new eye(199, 260, 100, 50, 1.0, "rightEye"),leftEye = new eye(399, 260, 100, 50, 1.0, "leftEye"); 
-    if(pattern == "パターン３") rightEye = new eye(199, 260, 50, 100, 1.0, "rightEye"),leftEye = new eye(399, 260, 50, 100, 1.0, "leftEye"); 
+    if (pattern == "パターン１") rightEye = new eye(canvasX / 2 - 100, 260, 50, 50, 1.0, "rightEye"), leftEye = new eye(canvasX / 2 + 100, 260, 50, 50, 1.0, "leftEye");
+    if (pattern == "パターン２") rightEye = new eye(canvasX / 2 - 100, 260, 60, 50, 1.0, "rightEye"), leftEye = new eye(canvasX / 2 + 100, 260, 60, 50, 1.0, "leftEye");
+    if (pattern == "パターン３") rightEye = new eye(canvasX / 2 - 100, 260, 50, 68, 1.0, "rightEye"), leftEye = new eye(canvasX / 2 + 100, 260, 50, 68, 1.0, "leftEye");
     eyePattern = pattern;
+    // if (pattern == "パターン１") rightEye = new eye(199, 260, 50, 50, 1.0, "rightEye"), leftEye = new eye(399, 260, 50, 50, 1.0, "leftEye");
+    // if (pattern == "パターン２") rightEye = new eye(199, 260, 100, 50, 1.0, "rightEye"), leftEye = new eye(399, 260, 100, 50, 1.0, "leftEye");
+    // if (pattern == "パターン３") rightEye = new eye(199, 260, 50, 100, 1.0, "rightEye"), leftEye = new eye(399, 260, 50, 100, 1.0, "leftEye");
+    // eyePattern = pattern;
   }
 
   draw() {
@@ -58,13 +72,13 @@ class eye {
   }
 
   resize(value, direction) {
-    if(value > this.eyeScale) {
+    if (value > this.eyeScale) {
       if (direction == "horizontal") {
         this.width = this.width * value / this.eyeScale;
         this.height = this.height * value / this.eyeScale;
       }
       // if (direction == "vertical") this.width = this.width * value / this.eyeScale, this.height = this.height * value / this.eyeScale;
-    }else if(value < this.eyeScale) {
+    } else if (value < this.eyeScale) {
       if (direction == "horizontal") {
         this.width = this.width * value / this.eyeScale;
         this.height = this.height * value / this.eyeScale;
@@ -77,9 +91,10 @@ class eye {
   repupilSize(value) {
     this.pupilScale = value;
   }
-  
+
 }
 
+// .1.b Mouse
 class mouse {
   constructor(x, y, width, height, mouseScale) {
     this.x = x;
@@ -90,16 +105,16 @@ class mouse {
   }
 
   init(pattern) {
-    if(pattern == "パターン１"); 
-    if(pattern == "パターン２"); 
-    if(pattern == "パターン３"); 
+    if (pattern == "パターン１");
+    if (pattern == "パターン２");
+    if (pattern == "パターン３");
     mousePattern = pattern;
-    console.log(pattern);
+    // console.log(pattern);
   }
 
   draw() {
     fill('#222222');
-      rect((canvasX - this.width) / 2, this.y, this.width, this.height);
+    rect((canvasX - this.width) / 2, this.y, this.width, this.height);
     // if(mousePattern == "パターン１") {
     //   fill('#222222');
     //   rect((canvasX - this.width) / 2, this.y, this.width, this.height);
@@ -112,10 +127,10 @@ class mouse {
   }
 
   resize(value) {
-    if(value > this.mouseScale) {
+    if (value > this.mouseScale) {
       this.width = this.width * value / this.mouseScale;
       this.height = this.height * value / this.mouseScale;
-    }else if(value < this.mouseScale) {
+    } else if (value < this.mouseScale) {
       this.width = this.width * value / this.mouseScale;
       this.height = this.height * value / this.mouseScale;
     }
@@ -123,67 +138,123 @@ class mouse {
   }
 }
 
-// 目
+// .1.c Head
+class head {
+  constructor(x, y, width, height, headRd) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.headRd = headRd
+  }
+
+  init(pattern) {
+    if (pattern == "パターン１");
+    if (pattern == "パターン２");
+    if (pattern == "パターン３");
+    headPattern = pattern;
+  }
+
+  draw() {
+    fill('#fcfcfc');
+
+    // pattern 1 : human face
+    if (headPattern == "パターン１") {
+      beginShape();
+      vertex(299, 20);
+      bezierVertex(520, 40, 500, 180, 500, 260);
+      bezierVertex(500, 260, 500, 440, 299, 500);
+      bezierVertex(299, 500, 98, 440, 98, 260);
+      bezierVertex(98, 180, 78, 40, 299, 20);
+      endShape();
+    }
+
+    // pattern 2 : NAO face
+    if (headPattern == "パターン２") {
+      fill('#fcfcfc');
+      strokeWeight(0);
+      rect(49, 140, 80, 240, 20, 20, 20, 20);
+      rect(469, 140, 80, 240, 20, 20, 20, 20);
+      ellipse(299, 260, 480, 420);
+      strokeWeight(1);
+    }
+
+    // pattern 3 : Musio X face
+    if (headPattern == "パターン３") {
+      beginShape();
+      vertex(299, 80);
+      bezierVertex(540, 96, 520, 240, 540, 480);
+      vertex(58, 480);
+      bezierVertex(78, 240, 58, 96, 299, 80);
+      endShape();
+    }
+  }
+
+  move(direction) {
+    if (direction == "up") this.y -= 10;
+    if (direction == "down") this.y += 10;
+  }
+}
+
+/* -- 1.x Global variable -- */
+
+// .x.a Eye
 let eyePattern = "";
-let rightEye = new eye(199, 260, 50, 50, 1.0, "rightEye");  // 右目
-let leftEye = new eye(399, 260, 50, 50, 1.0, "leftEye");    // 左目
+let rightEye = new eye(canvasX / 2 - 100, 260, 50, 50, 1.0, "rightEye");  // 右目
+let leftEye = new eye(canvasX / 2 + 100, 260, 50, 50, 1.0, "leftEye");    // 左目
 
-// 口
+// .x.b Mouse
 let mousePattern = "";
-let mouseM = new mouse((canvasX - 400)/2, 400, 20, 20, 1.0);
-// let mouthW = 20;
-// let mouthH = 20;
-// let mouthY = 400;
+let mouseM = new mouse((canvasX - 400) / 2, 400, 20, 20, 1.0);
 
-// 頭
-let headW = 400;
-let headH = 400;
-let headY = 60;
-let headRd = 16;
+// .x.c Head
+let headPattern = "";
+let robotHead = new head((canvasX - 400) / 2, 60, 400, 400, 16);
 
+// .x.x 
 let selected = "";
 
 
 
+/*** -- 2. function of p5.js -- ***/
+/* 
+2.1 setup() 
+2.2 draw()
+2.3 keyPressed()
+2.4 mouseReleased()
+2.5 mouseDragged()
+*/
 
-/*** -- 2. function of p5.js -- ***/ 
-// SETUP：最初に１回だけ(初期値)
+// 2.1 SETUP：最初に１回だけ(初期化)
 function setup() {
-  // キャンバスの作成
-  let cnvs = createCanvas(canvasX, canvasY);
-  cnvs.parent('canvas');
-
-  // 背景色の設定
-  background(canvasColor);
+  let cnvs = createCanvas(canvasX, canvasY); // Creating a Canvas
+  cnvs.parent('canvas'); // 親要素の変更
 }
 
-// DRAW：setup後に繰り返し実行（フレーム単位）
+// 2.2 DRAW：setup後に繰り返し実行（フレーム単位）
 function draw() {
   clear();
   background(canvasColor);
 
-  //頭
-  fill('#FFF');
-  rect((canvasX - headW) / 2, headY, headW, headH, headRd, headRd, headRd, headRd);
-
-  //口
-  // fill('#222222');
-  // rect((canvasX - mouthW) / 2, mouthY, mouthW, mouthH);
+  // Depiction of the head
+  robotHead.draw();
   mouseM.draw();
-
-  // 目
   rightEye.draw();  // 右目
   leftEye.draw();   // 左目
-  
+
   //selectの変更
   var eyeListItem = document.getElementById("eye-list-item");
-  changedPattern(eyeListItem.innerHTML,"eye");
+  changedPattern(eyeListItem.innerHTML, "eye");
   var mouseListItem = document.getElementById("mouse-list-item");
-  changedPattern(mouseListItem.innerHTML,"mouse")
+  changedPattern(mouseListItem.innerHTML, "mouse");
+  var headListItem = document.getElementById("head-list-item");
+  changedPattern(headListItem.innerHTML, "head");
+
+  tabSwiching();
 }
 
 
-// KEYPRESSED：キーが押された場合
+// 2.3 KEYPRESSED：キーが押された場合
 function keyPressed() {
   // 目
   if (selected == "eye") {
@@ -193,26 +264,23 @@ function keyPressed() {
     if (keyCode === LEFT_ARROW) leftEye.move("left"), rightEye.move("left");
   }
 
-  // 頭
-  if (selected == "head") {
-    if (keyCode === UP_ARROW) headY -= 10;
-    if (keyCode === DOWN_ARROW) headY += 10;
-  }
-
   // 口
   if (selected == "mouse") {
-    // if (keyCode === UP_ARROW) mouthY -= 10;
-    // if (keyCode === DOWN_ARROW) mouthY += 10;
     if (keyCode === UP_ARROW) mouseM.move("up");
     if (keyCode === DOWN_ARROW) mouseM.move("down");
   }
 
+  // Head
+  if (selected == "head") {
+    if (keyCode === UP_ARROW) robotHead.move("up");
+    if (keyCode === DOWN_ARROW) robotHead.move("down");
+  }
+
   return false;
-  
 }
 
 
-// MOOUSE_RELEASED：マウスがリリースされた場合
+// 2.4 MOOUSE_RELEASED：マウスがリリースされた場合
 function mouseReleased() {
   // 目のスライドバー
   var width = document.getElementById("slider-h").getAttribute("aria-valuenow");
@@ -229,29 +297,54 @@ function mouseReleased() {
 
 }
 
-// MOUSE_DRAGGED：マウスがドラッグされている間
+// 2.5 MOUSE_DRAGGED：マウスがドラッグされている間
 function mouseDragged() {
   if (selected == "eye") {
     leftEye.x = mouseX, leftEye.y = mouseY;
-    rightEye.x = 299 + (299 - mouseX), rightEye.y = mouseY;
+    // rightEye.x = 299 + (299 - mouseX), rightEye.y = mouseY;
+    rightEye.x = canvasX / 2 + (canvasX / 2 - mouseX), rightEye.y = mouseY;
   }
 }
 
 
-/*** -- 3. Original functions -- ***/ 
+/*** -- 3. Original functions -- ***/
 // SELECT_ELEMENT：移動する要素を選択する関数
 function selectElement(name) {
   selected = name;
 }
 
+// CHANGED_PATTERN：セレクタによりパターンを変更する関数
 function changedPattern(pattern, element) {
   // 目 
-  if(element == "eye") {
-    if(eyePattern != pattern) leftEye.init(pattern), rightEye.init(pattern);
+  if (element == "eye") {
+    if (eyePattern != pattern) leftEye.init(pattern), rightEye.init(pattern);
   }
 
   // 口 
-  if(element == "mouse") {
-    if(mousePattern != pattern) mouseM.init(pattern);
+  if (element == "mouse") {
+    if (mousePattern != pattern) mouseM.init(pattern);
   }
+
+  // Head 
+  if (element == "head") {
+    if (headPattern != pattern) robotHead.init(pattern);
+  }
+}
+
+// TAB_SWICHING：タブの遷移時にコンテンツを切り替える関数
+function tabSwiching() {
+  const content1 = document.getElementById("content1");
+  const tab1 = document.getElementById("mdc-tab-1").getAttribute("aria-selected");
+  if (tab1 != "true") content1.classList.remove("active", "edit-container"), content1.classList.add("disable");
+  if (tab1 == "true") content1.classList.remove("disable"), content1.classList.add("active", "edit-container");
+
+  const content2 = document.getElementById("content2");
+  const tab2 = document.getElementById("mdc-tab-2").getAttribute("aria-selected");
+  if (tab2 != "true") content2.classList.remove("active"), content2.classList.add("disable");
+  if (tab2 == "true") content2.classList.remove("disable"), content2.classList.add("active");
+
+  const content3 = document.getElementById("content3");
+  const tab3 = document.getElementById("mdc-tab-3").getAttribute("aria-selected");
+  if (tab3 != "true") content3.classList.remove("active"), content3.classList.add("disable");
+  if (tab3 == "true") content3.classList.remove("disable"), content3.classList.add("active");
 }
